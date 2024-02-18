43d925f896972ea2f6e0f8049b7fccf846dbb242
dennis iluma <dennis.iluma@sproxil.com>
2024-02-18 21:29:49 +0100

    added api to ge self current logged in user details

--- src/main/java/com/twd/SpringSecurityJWT/controller/AdminUsers.java
+++ src/main/java/com/twd/SpringSecurityJWT/controller/AdminUsers.java
@@ -5,6 +5,8 @@ import com.twd.SpringSecurityJWT.entity.Product;
 import com.twd.SpringSecurityJWT.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,5 +39,15 @@ public class AdminUsers {
     @GetMapping("/adminuser/both")
     public ResponseEntity<Object> bothAdminaAndUsersApi(){
         return ResponseEntity.ok("Both Admin and Users Can  access the api");
+    }
+
+    /** You can use this to get the details(name,email,role,ip, e.t.c) of user accessing the service*/
+    @GetMapping("/public/email")
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication); //get all details(name,email,password,roles e.t.c) of the user
+        System.out.println(authentication.getDetails()); // get remote ip
+        System.out.println(authentication.getName()); //returns the email because the email is the unique identifier
+        return authentication.getName(); // returns the email
     }
 }
